dbd9b089c492bde66f815e23f6c0a882ec37ab76
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-06-29 17:07:26 -0300

    desafio padrao de projeto

---
+++ desafioPadraoDeProjeto/src/SingletonLazy.java
@@ -0,0 +1,14 @@
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+
+    private SingletonLazy(){
+
+    }
+
+    public static SingletonLazy getInstancia(){
+        if(instancia == null){
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+}
---
+++ desafioPadraoDeProjeto/src/SingletonLazyHolder.java
@@ -0,0 +1,14 @@
+public class SingletonLazyHolder {
+    private static class InstanceHolder {
+        private static SingletonLazyHolder instancia = new SingletonLazyHolder();
+
+    }
+
+    private SingletonLazyHolder(){
+
+    }
+
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+}
